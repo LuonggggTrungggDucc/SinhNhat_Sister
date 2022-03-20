89f3c9f3f5a8e56853ac39cc6d1d7e6cf9753eb5
LuonggggTrungggDucc <ducduc27112k1@gmail.com>
2022-03-20 23:06:58 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Chị';
+const giftUrl = 'https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/276155227_310442461181977_4089451964198260639_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=Jrtb22HdeScAX9QVEoT&_nc_ht=scontent-sjc3-1.xx&oh=00_AT-MctN8xBFTPsQhP_537Fip8XJIAMhqUPUQXNT4nXK5YA&oe=623C831C';
+const eventName = 'Ấn vào hộp quà,';
+const titleCard = '“One wish for you on your birthday, whatever you ask you will receive, whatever you seek you may find, whatever you wish it will be fulfilled. on my sister is birthday Happy birthday to my sister';
+const contentCard = 'Enjoy your sister is day. Always fun and happy. Keep smiling 💘';
+
+
+const giftImage = 'anhchi.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
